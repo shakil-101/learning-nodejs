e759780d0d0cbfe06cfb293b884a4fd1f7377ce5
shakil <mdshakil.contact@gmail.com>
2024-03-15 14:24:19 +0600

    :construction: added to middleware in userModel to hash the password

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
+const bcrypt = require('bcryptjs');
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -33,5 +35,13 @@ const userSchema = new mongoose.Schema({
     }
   }
 });
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
